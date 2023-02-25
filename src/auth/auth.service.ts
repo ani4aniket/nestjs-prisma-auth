38b04b67e7955c91b767e655af30bb78d6d24303
Aniket Kumar <aniketkumar049@gmail.com>
2023-02-25 22:50:15 +0530

    Add signup route

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,5 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { PrismaService } from 'prisma/prisma.service';
 import { AuthDto } from './dto/auth.dto';
 
@@ -15,12 +16,46 @@ export class AuthService {
       throw new BadRequestException('Email already exists');
     }
 
+    const hashedPassword = await this.hashPassword(password);
+
+    await this.prisma.user.create({
+      data: { email, hashedPassword },
+    });
+
     return { message: 'signup was successful' };
   }
-  async signin() {
+  async signin(dto: AuthDto) {
+    const { email, password } = dto;
+
+    const foundUser = await this.prisma.user.findUnique({ where: { email } });
+
+    if (!foundUser) {
+      throw new BadRequestException('Wrong credentials');
+    }
+
+    const isMatch = await this.comparePasswords({
+      password,
+      hash: foundUser.hashedPassword,
+    });
+
+    if (!isMatch) {
+      throw new BadRequestException('Wrong credentials');
+    }
+
+    //sign jwt and return to the user
+
     return { message: 'signin was successful' };
   }
   async signout() {
     return { message: 'signout was successful' };
+  }
+
+  async hashPassword(password: string) {
+    const saltOrRounds = 10;
+    return await bcrypt.hash(password, saltOrRounds);
+  }
+
+  async comparePasswords(args: { password: string; hash: string }) {
+    return await bcrypt.compare(args.password, args.hash);
   }
 }
